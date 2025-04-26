d779eca4b76b54503bd1d4ad4342e3fadd5b3f24
Aoichaan0513 <aoichaan0513@gmail.com>
2025-04-26 20:43:40 +0900

    [Update] テストファイルの書き換え

--- test/test-parse.ts
+++ test/test-parse.ts
@@ -25,26 +25,26 @@ describe("markdown", () => {
           doc(p("hello!"))))
 
   it("parses headings", () =>
-     same("# one\n\n## two\n\nthree",
+     same("# one\n## two\nthree",
           doc(h1("one"), h2("two"), p("three"))))
 
   it("parses a blockquote", () =>
-     same("> once\n\n> > twice",
+     same("> once\n> > twice",
           doc(blockquote(p("once")), blockquote(blockquote(p("twice"))))))
 
   // FIXME bring back testing for preserving bullets and tight attrs
   // when supported again
 
   it("parses a bullet list", () =>
-     same("* foo\n\n  * bar\n\n  * baz\n\n* quux",
+     same("* foo\n  * bar\n  * baz\n* quux",
           doc(ul(li(p("foo"), ul(li(p("bar")), li(p("baz")))), li(p("quux"))))))
 
   it("parses an ordered list", () =>
-     same("1. Hello\n\n2. Goodbye\n\n3. Nest\n\n   1. Hey\n\n   2. Aye",
+     same("1. Hello\n2. Goodbye\n3. Nest\n   1. Hey\n   2. Aye",
           doc(ol(li(p("Hello")), li(p("Goodbye")), li(p("Nest"), ol(li(p("Hey")), li(p("Aye"))))))))
 
   it("preserves ordered list start number", () =>
-     same("3. Foo\n\n4. Bar",
+     same("3. Foo\n4. Bar",
           doc(ol3(li(p("Foo")), li(p("Bar"))))))
 
   it("can parse a heading in a list", () =>
@@ -52,15 +52,15 @@ describe("markdown", () => {
          doc(ul(li(h1("Foo"))))))
 
   it("parses a code block", () =>
-     same("Some code:\n\n```\nHere it is\n```\n\nPara",
+     same("Some code:\n```\nHere it is\n```\nPara",
           doc(p("Some code:"), schema.node("code_block", {params: ""}, [schema.text("Here it is")]), p("Para"))))
 
   it("parses an intended code block", () =>
-     parse("Some code:\n\n    Here it is\n\nPara",
+     parse("Some code:\n    Here it is\nPara",
           doc(p("Some code:"), pre("Here it is"), p("Para"))))
 
   it("parses a fenced code block with info string", () =>
-     same("foo\n\n```javascript\n1\n```",
+     same("foo\n```javascript\n1\n```",
           doc(p("foo"), schema.node("code_block", {params: "javascript"}, [schema.text("1")]))))
 
   it("parses inline marks", () =>
@@ -132,7 +132,7 @@ describe("markdown", () => {
           doc(p("line one", br(), "line two"))))
 
   it("parses a horizontal rule", () =>
-     same("one two\n\n---\n\nthree",
+     same("one two\n---\nthree",
           doc(p("one two"), hr(), p("three"))))
 
   it("ignores HTML tags", () =>
@@ -167,7 +167,7 @@ describe("markdown", () => {
   })
 
   it("doesn't put a code block after a list item inside the list item", () =>
-     same("* list item\n\n```\ncode\n```",
+     same("* list item\n```\ncode\n```",
           doc(ul({tight: true}, li(p("list item"))), pre("code"))))
 
   it("doesn't escape characters in code", () =>
@@ -215,11 +215,11 @@ describe("markdown", () => {
   })
 
   it("escapes list markers inside lists", () => {
-    same("* 1\\. hi\n\n* x", doc(ul(li(p("1. hi")), li(p("x")))))
+    same("* 1\\. hi\n* x", doc(ul(li(p("1. hi")), li(p("x")))))
   })
 
   it("does not escape list markers in the middle of paragraphs", () => {
-    same("123 [0. com](foo)\n\n123 [2. 2](foo)",
+    same("123 [0. com](foo)\n123 [2. 2](foo)",
          doc(p("123 ", a("0. com")), p("123 ", a("2. 2"))))
   })
 
